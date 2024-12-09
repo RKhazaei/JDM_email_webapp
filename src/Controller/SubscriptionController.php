4edd81e45890b7c1134521f5cec5b3be2255b9ba
RKhazaei <302769656@student.rocmondriaan.nl>
2024-12-10 00:54:57 +0100

    first version made non functional

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -10,26 +10,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Mailer\MailerInterface; // Import the mailer interface
+use Symfony\Component\Mime\Email as MimeEmail;// Import the email class
 
 class SubscriptionController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
     private ValidatorInterface $validator;
+    private MailerInterface $mailer; // Add MailerInterface to your dependencies
 
-    public function __construct(EntityManagerInterface $entityManager, ValidatorInterface $validator)
+    public function __construct(EntityManagerInterface $entityManager, ValidatorInterface $validator, MailerInterface $mailer)
     {
         $this->entityManager = $entityManager;
         $this->validator = $validator;
+        $this->mailer = $mailer; // Initialize the mailer service
     }
 
     #[Route('/subscribe', name: 'subscribe', methods: ['GET', 'POST'])]
     public function subscribe(Request $request): Response
     {
         if ($request->isMethod('POST')) {
-            $email = $request->request->get('email');
+            $emailAddress = $request->request->get('email');
 
             // Validate the email
-            $errors = $this->validator->validate($email, [
+            $errors = $this->validator->validate($emailAddress, [
                 new Assert\NotBlank(),
                 new Assert\Email(),
             ]);
@@ -37,19 +41,23 @@ class SubscriptionController extends AbstractController
             if (count($errors) > 0) {
                 $this->addFlash('error', "Invalid email address. Please try again.");
             } else {
-                $existingEmail = $this->entityManager->getRepository(Email::class)->findOneBy(['email' => $email]);
+                $existingEmail = $this->entityManager->getRepository(Email::class)->findOneBy(['email' => $emailAddress]);
                 
                 if ($existingEmail) {
                     $this->addFlash('error', "This email is already subscribed!");
                 } else {
                     // Persist email into the database
                     $subscription = new Email();
-                    $subscription->setEmail($email);
+                    $subscription->setEmail($emailAddress);
 
                     $this->entityManager->persist($subscription);
                     $this->entityManager->flush();
 
                     $this->addFlash('success', "Thank you for subscribing!");
+
+                    // Send confirmation email
+                    $this->sendConfirmationEmail($emailAddress);
+
                 }
             }
 
@@ -57,5 +65,21 @@ class SubscriptionController extends AbstractController
         }
 
         return $this->render('subscription/index.html.twig');
+    }
+
+    private function sendConfirmationEmail(string $emailAddress): void
+    {
+            $email = (new MimeEmail())
+                ->from('rezakhazaei0405@gmail.com') // Replace with your email
+                ->to($emailAddress)
+                ->subject('Subscription Confirmation')
+                ->html(
+                    $this->renderView(
+                        'emails/confirmation.html.twig',
+                        ['email' => $emailAddress]
+                    )
+                );
+
+            $this->mailer->send($email);
     }
 }
